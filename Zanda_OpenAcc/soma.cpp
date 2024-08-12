f8d789a588564682cb5b6e8201edbd8f535e24f8
gZanda <gabrielfzanda@gmail.com>
2024-08-12 11:47:24 -0300

    Update

--- Zanda_OpenAcc/soma.cpp
+++ Zanda_OpenAcc/soma.cpp
@@ -15,12 +15,16 @@ int main() {
         a[i] = i;
         b[i] = 2 * i;
     }
+
+    // Exibe o vetor a
     printf("Vetor a:\n");
-    for (int i = 0; i < 100; ++i) {
+    for (int i = 0; i < 10; ++i) { // Mostra apenas os primeiros 10 elementos para simplicidade
         std::cout << "a[" << i << "] = " << a[i] << std::endl;
     }
+
+    // Exibe o vetor b
     printf("Vetor b:\n");
-    for (int i = 0; i < 100; ++i) {
+    for (int i = 0; i < 10; ++i) { // Mostra apenas os primeiros 10 elementos para simplicidade
         std::cout << "b[" << i << "] = " << b[i] << std::endl;
     }
 
@@ -35,7 +39,7 @@ int main() {
 
     // Exibe os primeiros 10 resultados
     printf("Resultado:\n");
-    for (int i = 0; i < 100; ++i) {
+    for (int i = 0; i < 10; ++i) { // Mostra apenas os primeiros 10 elementos para simplicidade
         std::cout << "c[" << i << "] = " << c[i] << std::endl;
     }
 
