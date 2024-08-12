da060c3e8768a1b0600a0a69d3000d410b7f0f37
gZanda <gabrielfzanda@gmail.com>
2024-08-12 11:38:38 -0300

    Soma de Vetores

---
+++ Zanda_OpenAcc/soma.cpp
@@ -0,0 +1,36 @@
+#include <openacc.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <iostream>
+#include <vector>
+
+int main() {
+    const int N = 100; // Tamanho dos vetores
+
+    // Inicialização dos vetores
+    std::vector<int> a(N), b(N), c(N);
+
+    // Preenche os vetores a e b com alguns valores
+    for (int i = 0; i < N; ++i) {
+        a[i] = i;
+        b[i] = 2 * i;
+    }
+
+    // Soma dos vetores a e b em paralelo usando OpenACC
+    #pragma acc data copyin(a[0:N], b[0:N]) copyout(c[0:N])
+    {
+        #pragma acc parallel loop
+        for (int i = 0; i < N; ++i) {
+            c[i] = a[i] + b[i];
+        }
+    }
+
+    // Exibe os primeiros 10 resultados
+    std::cout << "Elementos:\n";
+    for (int i = 0; i < 100; ++i) {
+        std::cout << "c[" << i << "] = " << c[i] << std::endl;
+    }
+
+    return 0;
+}
